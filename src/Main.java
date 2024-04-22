807a43a0615a92c2bf2b761b78472f8a0f054af1
Pridius <darya.pridius@yandex.ru>
2024-04-22 19:04:24 +0300

    add Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 18700;
         int point = 20;
-        int bonus = price/point;
+        int bonus = price / point;
         System.out.println("Бонусные мили:" + bonus);
 
     }
